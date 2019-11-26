52fda40caab73e7fc742014d5ac7656c4c5464bc
Manorlds-Eaglespark <manorldsapiens@gmail.com>
2019-11-26 12:49:07 +0300

    initial commit

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+SECRET_KEY = os.urandom(32)
+# Grabs the folder where the script runs.
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Enable debug mode.
+DEBUG = True
+
+# TODO IMPLEMENT DATABASE URL
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234@localhost/fyyur'
